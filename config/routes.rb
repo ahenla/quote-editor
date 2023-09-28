f2ac6f929720596cdbd20355bf43672d9d237626
Andres Henriquez <rymbell@gmail.com>
2023-09-28 15:19:39 +0900

    added crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :quotes do
-    resources :line_item_dates, except: [:index, :show]
+    resources :line_item_dates, except: [:index, :show] do
+      resources :line_items, except: [:index, :show]
+    end
   end
   # Defines the root path route ("/")
   # root "articles#index"
